0b3a8e42bc1349bd8a4c0bfa9c975a5599bdd498
emily <emilydkiehl@gmail.com>
2020-02-23 04:55:29 -0500

    Save user inputs

--- app/src/main/java/com/example/nutritiononabudget/MainActivity.java
+++ app/src/main/java/com/example/nutritiononabudget/MainActivity.java
@@ -1,17 +1,59 @@
 package com.example.nutritiononabudget;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.widget.TextView;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //final TextView calories = findViewById(R.id.calories);
+        final EditText totalCaloriesView = findViewById(R.id.total_calories);
+        final EditText totalDollarsView = findViewById(R.id.total_dollars);
+        Button saveCalories = findViewById(R.id.save_calories);
+        saveCalories.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                int totalCalories = Integer.parseInt(totalCaloriesView.getText().toString());
+                System.out.println(totalCalories);
+            }
+        });
+        Button saveDollars = findViewById(R.id.save_dollars);
+        saveDollars.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int totalDollars = Integer.parseInt(totalDollarsView.getText().toString());
+                System.out.println(totalDollars);
+            }
+        });
+        Button searchBtn = findViewById(R.id.search_btn);
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Connection c = null;
+
+                try {
+                    Class.forName("org.sqlite.JDBC");
+                    c = DriverManager.getConnection("jdbc:sqlite:test.db");
+                } catch ( Exception e ) {
+                    System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                    System.exit(0);
+                }
+                System.out.println("Opened database successfully");
+            }
+        });
     }
 }
+
---
+++ app/src/main/java/com/example/nutritiononabudget/ResultsActivity.java
@@ -0,0 +1,21 @@
+package com.example.nutritiononabudget;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import com.github.mikephil.charting.charts.BarChart;
+//import com.github.mikephil.charting.data.BarData;
+//import com.github.mikephil.charting.data.BarDataSet;
+//import com.github.mikephil.charting.data.BarEntry;
+//import com.github.mikephil.charting.utils.ColorTemplate;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+
+    }
+}
